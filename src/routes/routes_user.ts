17de5ee34132e08326d2251dffbf59c78d54ba1e
Muhammad Dimas Renaldi <mdrdimas1@gmail.com>
2026-05-03 18:56:25 +0700

    cek nik sudah absen / belum

--- src/routes/routes_user.ts
+++ src/routes/routes_user.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { User } from "../models/user.js";
+import { Day1 } from "../models/data.js";
 
 const routerUser = Router();
 
@@ -22,6 +23,23 @@ routerUser.get('/:nik', async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Server Error', error });
 
     }
-}) 
+})
+
+routerUser.get('/cek-absen/:nik', async (req: Request, res: Response) => {
+    try {
+        const { nik } = req.params;
+
+        // Cari apakah NIK tersebut sudah ada di koleksi Day1 dan Day2
+        const absenDay1 = await Day1.findOne({ nik: String(nik) })
+
+        // Kembalikan statusnya (true jika sudah ada, false jika belum)
+        res.status(200).json({
+            day1: absenDay1 !== null
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Gagal mengecek status absen" });
+    }
+});
 
 export default routerUser;
