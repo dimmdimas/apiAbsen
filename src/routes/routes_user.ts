9bbd80ce7146d5b6305b1d4b36f95ac9e4104929
Muhammad Dimas Renaldi <mdrdimas1@gmail.com>
2026-06-01 19:14:12 +0700

    fix cek absen user

--- src/routes/routes_user.ts
+++ src/routes/routes_user.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { User } from "../models/user.js";
-import { Day1 } from "../models/data.js";
+import { Day1, Day2 } from "../models/data.js";
 
 const routerUser = Router();
 
@@ -29,12 +29,14 @@ routerUser.get('/cek-absen/:nik', async (req: Request, res: Response) => {
     try {
         const { nik } = req.params;
 
-        // Cari apakah NIK tersebut sudah ada di koleksi Day1 dan Day2
-        const absenDay1 = await Day1.findOne({ nik: String(nik) })
+        const cekDay1 = await Day1.findOne({ nik: String(nik) })
+
+        const cekDay2 = await Day2.findOne({ nik: String(nik) });
 
         // Kembalikan statusnya (true jika sudah ada, false jika belum)
         res.status(200).json({
-            day1: absenDay1 !== null
+            day1: cekDay1 ? true : false,
+            day2: cekDay2 ? true : false 
         });
     } catch (error) {
         console.error(error);
